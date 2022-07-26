ac012e06c50f2e0c9311bd0508058aa13f97400d
Ratheesh PR <ratheesh.pr@techversantinfo.com>
2022-07-26 10:37:07 +0530

    production mail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Product.create(name:"Chilly Powder",price_per_kg: 450)
-ProductPack.create(pack_weight: 100, weight_type: "G", product_id: 1)
-ProductPack.create(pack_weight: 250, weight_type: "G", product_id: 1)
-ProductPack.create(pack_weight: 500, weight_type: "G", product_id: 1)
-ProductPack.create(pack_weight: 1, weight_type: "Kg", product_id: 1)
-
-Product.create(name:"Pickle Powder",price_per_kg: 420)
-ProductPack.create(pack_weight: 100, weight_type: "G", product_id: 2)
-ProductPack.create(pack_weight: 250, weight_type: "G", product_id: 2)
-
-Product.create(name:"Pepper Powder",price_per_kg: 750)
-ProductPack.create(pack_weight: 100, weight_type: "G", product_id: 3)
-ProductPack.create(pack_weight: 250, weight_type: "G", product_id: 3)
-ProductPack.create(pack_weight: 500, weight_type: "G", product_id: 3)
-ProductPack.create(pack_weight: 1, weight_type: "Kg", product_id: 3)
+# Product.create(name:"Chilly Powder",price_per_kg: 450)
+# ProductPack.create(pack_weight: 100, weight_type: "G", product_id: 1)
+# ProductPack.create(pack_weight: 250, weight_type: "G", product_id: 1)
+# ProductPack.create(pack_weight: 500, weight_type: "G", product_id: 1)
+# ProductPack.create(pack_weight: 1, weight_type: "Kg", product_id: 1)
+#
+# Product.create(name:"Pickle Powder",price_per_kg: 420)
+# ProductPack.create(pack_weight: 100, weight_type: "G", product_id: 2)
+# ProductPack.create(pack_weight: 250, weight_type: "G", product_id: 2)
+#
+# Product.create(name:"Pepper Powder",price_per_kg: 750)
+# ProductPack.create(pack_weight: 100, weight_type: "G", product_id: 3)
+# ProductPack.create(pack_weight: 250, weight_type: "G", product_id: 3)
+# ProductPack.create(pack_weight: 500, weight_type: "G", product_id: 3)
+# ProductPack.create(pack_weight: 1, weight_type: "Kg", product_id: 3)
