560db6fb1cbd5e49d5b6b9b1a27d2715974bc812
ratheeshtechversant <103477827+ratheeshtechversant@users.noreply.github.com>
2022-07-27 15:52:50 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'https://shopping-app-sample.herokuapp.com',
+  domain:               'shopping-app-sample.herokuapp.com',
   user_name:            'ratheesh.pr@techversantinfo.com',
   password:             'bllwtrqmjouhifob',
   authentication:       'plain',
