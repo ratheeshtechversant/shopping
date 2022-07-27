da98d8664374da600c4c7f375c74f654843f0021
ratheeshtechversant <103477827+ratheeshtechversant@users.noreply.github.com>
2022-07-27 15:46:43 +0530

    Update production.rb
    mailer service added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'https://shopping-app-sample.herokuapp.com' ,PORT:-5000 }
   
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'https://shopping-app-sample.herokuapp.com',
+  user_name:            'ratheesh.pr@techversantinfo.com',
+  password:             'bllwtrqmjouhifob',
+  authentication:       'plain',
+  enable_starttls_auto: true,
+  open_timeout:         5,
+  read_timeout:         5 }
+
 end
